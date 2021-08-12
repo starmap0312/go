dad57812d73901caa3f5699e2d2ccc037203d1d2
kuanyu <kuanyu@oath.com>
2021-08-12 16:58:40 +0800

    more

--- gotour.go
+++ gotour.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"time"
 
 	"rsc.io/quote"
 ) // use external package
@@ -78,14 +79,84 @@ type Vertex struct { // a named struct
 // struct method: Go does not have classe, but you can define methods on struct
 func (v Vertex) sum() int { // define a method of Vertex v
 	// note that the method operates on a copy of the original Vertex value
+	// i.e. this will copy the value on each method call (less efficient)
 	return v.x + v.y
 }
 
-// struct method: Go does not have classe, but you can define methods on struct
+// struct method: Go does not have class, but you can use struct's pointer receiver to modify the struct's fields
 func (v *Vertex) double() { // define a method of Vertex v
-	// note that use a pointer receiver (*Vertex) so that the method can change the Vertex value
+	// note that if you do not use a pointer receiver (*Vertex), the method will operate on a copy of the original Vertex value
 	v.x = v.x * 2
 	v.y = v.y * 2
+}
+
+// interfaces: an interface type is defined as a set of method signatures
+type Animal interface {
+	run()
+}
+
+type Dog struct {
+	name string
+}
+
+// type Dog implements the interface Animal, but we don't need to explicitly declare that it does so
+//   instead, we do that by declaring a Dog instance as type interface Animal, which will enforce the Dog type to implement the interface method
+//   ex. var dog Animal = Dog{"john's dog"}
+func (d Dog) run() {
+	fmt.Println(d.name + " is running.")
+}
+
+type DogPtr struct {
+	name string
+}
+
+func (d *DogPtr) run() {
+	fmt.Println(d.name + " is running.")
+}
+
+// built-in interface: fmt.Stringer
+//   type Stringer interface {
+// 	   String() string
+//   }
+// implement built-in interface: fmt.Stringer
+type IPAddr [4]byte
+
+func (v IPAddr) String() string {
+	return fmt.Sprintf("%d.%d.%d.%d", v[0], v[1], v[2], v[3])
+}
+
+// built-in interface: error
+//   type error interface {
+//   	Error() string
+//   }
+// implement built-in interface: error
+type MyError struct {
+	msg string
+}
+
+func (err *MyError) Error() string {
+	return fmt.Sprintf("message: %s", err.msg)
+}
+
+func funcThrowsMyError() error {
+	return &MyError{"error inside funcThrowsMyError"}
+}
+
+// goroutine: a lightweight thread managed by the Go runtime
+func heavyTask(elapsed int) {
+	fmt.Printf("started heavy task: %d\n", elapsed)
+	time.Sleep(time.Duration(elapsed*1000) * time.Millisecond)
+	fmt.Printf("finished heavy task: %d\n", elapsed)
+}
+
+// channels: a typed conduit through which you can send and receive values with the channel operator, <-
+// ex. ch <- v    // send v to channel ch
+//     v := <-ch  // receive from ch and assign the value to v
+func send(elapsed int, ch chan int) {
+	fmt.Printf("started sending: %d\n", elapsed)
+	time.Sleep(time.Duration(elapsed*1000) * time.Millisecond)
+	ch <- elapsed // send 100 to ch
+	fmt.Printf("finished sending: %d\n", elapsed)
 }
 
 func main() {
@@ -144,7 +215,7 @@ func main() {
 	v.x = 10
 	fmt.Println(v) // {10 2}
 
-	// struct method: Go does not have classe, but you can define methods on struct
+	// struct method: Go does not have class, but you can define methods on struct
 	fmt.Println(v.sum()) // 12
 	v.double()
 	fmt.Println(v) // {20 4}
@@ -175,9 +246,20 @@ func main() {
 	}
 	fmt.Println(board) // [[_ _ _] [_ _ _] [_ _ _]]
 
+	// iterate an array
+	nums := [6]int{1, 2, 3, 4, 5, 6} // an array of integers
+	for i, v := range nums {
+		fmt.Println(i, v)
+	}
+	// 0 1
+	// 1 2
+	// 2 3
+	// 3 4
+	// 4 5
+	// 5 6
+
 	// Slices: it is like a reference to the underlying array, i.e. a slice does not store any data
 	//   an array has a fixed size; a slice is a dynamically-sized view into the elements of an array
-	nums := [6]int{1, 2, 3, 4, 5, 6}          // an array of integers
 	fmt.Println(nums)                         // [1 2 3 4 5 6]
 	fmt.Println(nums[1:])                     // [2 3 4 5 6]
 	fmt.Println(nums[:1])                     // [1]
@@ -225,6 +307,15 @@ func main() {
 	var newNumPtr *int = new(int) // newNumPtr has type: *int
 	fmt.Println(*newNumPtr)       // 0
 
+	// iterate a Map
+	mp2 := map[string]int{
+		"key1": 1,
+		"key2": 2,
+	}
+	for k, v := range mp2 {
+		fmt.Printf("%v: %v\n", k, v) // key1: 1 & key2: 2
+	}
+
 	// Function values: pass a function just like other values
 	var result = double(add, 1, 2)
 	fmt.Println(result) // add(1, 2) * 2 = 6
@@ -232,6 +323,84 @@ func main() {
 	// Function closures: a function value that references variables from outside its body
 	fn := addOne()
 	fmt.Println(fn(10)) // 11
+
+	// interfaces
+	var dog Animal = Dog{"john's dog"}
+	fmt.Printf("(%v, %T)\n", dog, dog) // ({john's dog}, main.Dog)
+	// note: interface values can be thought of as a tuple of a value and a concrete type: (value, type)
+	dog.run() // john's dog is running
+
+	var dogPtr Animal = &DogPtr{"john's dogPtr"}
+	// note: if you do not wirte &DogPtr here, you will receive the following compile error:
+	//   DogPtr does not implement Animal (run method has pointer receiver)
+	fmt.Printf("(%v, %T)\n", dogPtr, dogPtr) // (&{john's dogPtr}, *main.DogPtr)
+	dogPtr.run()                             // john's dog is running
+
+	var nullPtr Animal
+	// nullPtr.run()
+	// note: if call a method on a nil interface, you will receive the following run-time error:
+	//   panic: runtime error: invalid memory address or nil pointer dereference
+	fmt.Printf("(%v, %T)\n", nullPtr, nullPtr) // (<nil>, <nil>)
+	// note: the nil interface has no concrete type inside the interface tuple to indicate which concrete method to call
+
+	// empty interface: it specifies zero methods
+	var emptyItf interface{}
+	fmt.Printf("(%v, %T)\n", emptyItf, emptyItf) // (<nil>, <nil>)
+
+	// type assertion: it provides access to an interface value's underlying concrete value
+	var itf interface{} = "abc"
+	value := itf.(string) // access the underlying concrete value as type string
+	fmt.Println(value)    // abc
+
+	value, ok := itf.(string)
+	fmt.Println(value, ok) // abc true
+
+	fail, ok := itf.(float64) // fails to access the underlying concrete value as type float64
+	fmt.Println(fail, ok)     // 0 false
+
+	// type switch: it is a construct that permits several type assertions in series.
+	switch v := itf.(type) {
+	case string:
+		fmt.Printf("string type matched: value=%v, type=%T\n", v, v) // string type matched: value=abc, type=string
+	case float64:
+		fmt.Printf("float64 type matched: value=%v, type=%T\n", v, v)
+	default:
+		fmt.Printf("no type matched: value=%v, type=%T\n", v, v)
+	}
+
+	ip := IPAddr{127, 0, 0, 1}
+	fmt.Printf("%v\n", ip) // 127.0.0.1
+
+	err := funcThrowsMyError()
+	fmt.Println(err) // message: error inside funcThrowsMyError
+
+	// goroutine: a lightweight thread managed by the Go runtime
+	go heavyTask(3) // this starts a new goroutine running
+	time.Sleep(100 * time.Millisecond)
+	heavyTask(1)
+	// started heavy task: 3
+	// started heavy task: 1
+	// finished heavy task: 1
+	// finished heavy task: 3
+	time.Sleep(3000 * time.Millisecond)
+
+	// channels: a typed conduit through which you can send and receive values with the channel operator, <-
+	ch := make(chan int)
+	go send(2, ch)
+	go send(3, ch)
+	go send(1, ch)
+	x, y, z := <-ch, <-ch, <-ch // receive from ch
+	// note: sends and receives block until the other side is ready
+	//       this allows goroutines to synchronize without explicit locks or condition variables
+
+	fmt.Println(x, y, z)
+	// started sending: 1
+	// started sending: 2
+	// started sending: 3
+	// finished sending: 1
+	// finished sending: 2
+	// finished sending: 3
+	// 1 2 3
 
 	// defer: deferred function calls are pushed onto a stack
 	//        when a function returns, its deferred calls are executed in last-in-first-out order
